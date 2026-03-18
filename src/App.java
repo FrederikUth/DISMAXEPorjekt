fd31b5ac965e18e297ba612469845b4e0c7b9505
FrederikUth <uthfrederik@gmail.com>
2026-03-18 09:32:30 +0100

    asd

--- src/App.java
+++ src/App.java
@@ -5,9 +5,19 @@ import javafx.application.Application;
 public class App {
 	public static Player me=null;
 	public static void main(String[] args) throws Exception{
-		BufferedReader inFromUser = new BufferedReader(new InputStreamReader(System.in));
-		System.out.println("Indtast spillernavn");
-		String navn = inFromUser.readLine();
+        String sentence;
+        String modifiedSentence;
+        BufferedReader inFromUser = new BufferedReader(new InputStreamReader(System.in));
+        Socket clientSocket= new Socket("localhost",6767);
+        DataOutputStream outToServer = new DataOutputStream(clientSocket.getOutputStream());
+        BufferedReader inFromServer = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+        System.out.println("Indtast spillernavn");
+        String navn = inFromUser.readLine();
+        outToServer.writeBytes(navn + '\n');
+        modifiedSentence = inFromServer.readLine();
+        System.out.println("FROM SERVER: " + modifiedSentence);
+        clientSocket.close();
+
 		me = GameLogic.makePlayers(navn);
 		Application.launch(Gui.class);
 	}
