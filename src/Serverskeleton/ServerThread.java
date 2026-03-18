dff7ed775d40ca3fd3681c436e3fec32f2397e70
FrederikUth <uthfrederik@gmail.com>
2026-03-18 11:18:58 +0100

    asd

--- src/Serverskeleton/ServerThread.java
+++ src/Serverskeleton/ServerThread.java
@@ -5,54 +5,54 @@ import Game.Player;
 import java.net.*;
 import java.io.*;
 public class ServerThread extends Thread{
-	Socket connSocket;
-	common c;
+    Socket connSocket;
+    common c;
     public static Player me = null;
-	
-	public ServerThread(Socket connSocket,common c) {
-		this.connSocket = connSocket;
-		this.c=c; // Til Web-server opgaven skal denne ikke anvendes
-	}
-	public void run() {
-		try {
-			BufferedReader inFromClient = new BufferedReader(new InputStreamReader(connSocket.getInputStream()));
-			DataOutputStream outToClient = new DataOutputStream(connSocket.getOutputStream());
-			
-			// Do the work and the communication with the client here	
-			// The following two lines are only an example
-			
-			String clientSentence = inFromClient.readLine();
-			outToClient.writeBytes("Hej"+ clientSentence + '\n' );
-           me = GameLogic.makePlayers(clientSentence);
-           Server.players.add(me);
 
-			while (true) {
-				String msg = inFromClient.readLine();
+    public ServerThread(Socket connSocket,common c) {
+        this.connSocket = connSocket;
+        this.c=c; // Til Web-server opgaven skal denne ikke anvendes
+    }
+    public void run() {
+        try {
+            BufferedReader inFromClient = new BufferedReader(new InputStreamReader(connSocket.getInputStream()));
+            DataOutputStream outToClient = new DataOutputStream(connSocket.getOutputStream());
 
-				if (msg == null) break;
+            // Do the work and the communication with the client here
+            // The following two lines are only an example
 
-				if (msg.startsWith("MOVE")) {
-					String direction = msg.split(" ")[1];
+            String clientSentence = inFromClient.readLine();
+            outToClient.writeBytes("Hej"+ clientSentence + '\n' );
+            me = GameLogic.makePlayers(clientSentence);
+            Server.players.add(me);
 
-					int dx = 0, dy = 0;
+            while (true) {
+                String msg = inFromClient.readLine();
 
-					switch (direction) {
-						case "up": dy = -1; break;
-						case "down": dy = 1; break;
-						case "left": dx = -1; break;
-						case "right": dx = 1; break;
-					}
+                if (msg == null) break;
 
-					GameLogic.updatePlayer(me, dx, dy, direction);
+                if (msg.startsWith("MOVE")) {
+                    String direction = msg.split(" ")[1];
 
-					// send update til klienter (du mangler broadcast)
-				}
-			}
-		
-		} catch (IOException e) {
-			e.printStackTrace();
-		}		
-		// do the work here
-	}
+                    int dx = 0, dy = 0;
+
+                    switch (direction) {
+                        case "up": dy = -1; break;
+                        case "down": dy = 1; break;
+                        case "left": dx = -1; break;
+                        case "right": dx = 1; break;
+                    }
+
+                    GameLogic.updatePlayer(me, dx, dy, direction);
+
+                    // send update til klienter (du mangler broadcast)
+                }
+            }
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        // do the work here
+    }
 }
 //hej
