71990a5a3a64e9e14e47ea3097312ad22e3f5033
FrederikUth <uthfrederik@gmail.com>
2026-03-20 09:12:24 +0100

    Det hele virker

--- src/Game/GameLogic.java
+++ src/Game/GameLogic.java
@@ -4,81 +4,72 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
 
-
-
 public class GameLogic {
-public static List<Player> players = new ArrayList<Player>();	
-	//public static Game.Player me;
-	
-	
-	public static Player makePlayers(String name) {
-		pair p=getRandomFreePosition();
-		Player me = new Player(name,p,"up");
-		players.add(me);
-		return me;
-	}
-	
-	public static pair getRandomFreePosition()
-	// finds a random new position which is not wall 
-	// and not occupied by other players 
-	{
-		int x = 1;
-		int y = 1;
-		boolean foundfreepos = false;
-		while  (!foundfreepos) {
-			Random r = new Random();
-			x = Math.abs(r.nextInt()%18) +1;
-			y = Math.abs(r.nextInt()%18) +1;
-			if (Generel.board[y].charAt(x)==' ') // er det gulv ?
-			{
-				foundfreepos = true;
-				for (Player p: players) {
-					if (p.getXpos()==x && p.getYpos()==y) //pladsen optaget af en anden 
-						foundfreepos = false;
-				}
-				
-			}
-		}
-		pair p = new pair(x,y);
-		return p;
-	}
-	
-	public static void updatePlayer(Player me,int delta_x, int delta_y, String direction)
-	{
-		me.direction = direction;
-		int x = me.getXpos(),y = me.getYpos();
+    public static List<Player> players = new ArrayList<Player>();
 
-		if (Generel.board[y+delta_y].charAt(x+delta_x)=='w') {
-			me.addPoints(-1);
-		} 
-		else {
-			// collision detection
-			Player p = getPlayerAt(x+delta_x,y+delta_y);
-			if (p!=null) {
-              me.addPoints(10);
-              //update the other player
-              p.addPoints(-10);
-              pair pa = getRandomFreePosition();
-              p.setLocation(pa);
-              pair oldpos = new pair(x+delta_x,y+delta_y);
-              Gui.movePlayerOnScreen(oldpos,pa,p.direction);
-			} else 
-				me.addPoints(1);
-			pair oldpos = me.getLocation();
-			pair newpos = new pair(x+delta_x,y+delta_y); 
-			Gui.movePlayerOnScreen(oldpos,newpos,direction);
-			me.setLocation(newpos);
-		}
-		
-		
-	}
-	
-	public static Player getPlayerAt(int x, int y) {
-		for (Player p : players) {
-			if (p.getXpos()==x && p.getYpos()==y) {
-				return p;
-			}
-		}
-		return null;
-	}
+    public static Player makePlayers(String name) {
+        pair p = getRandomFreePosition();
+        Player me = new Player(name, p, "up");
+        players.add(me);
+        return me;
+    }
+
+    public static pair getRandomFreePosition() {
+        int x = 1;
+        int y = 1;
+        boolean foundfreepos = false;
+        while (!foundfreepos) {
+            Random r = new Random();
+            x = Math.abs(r.nextInt() % 18) + 1;
+            y = Math.abs(r.nextInt() % 18) + 1;
+            if (Generel.board[y].charAt(x) == ' ') // er det gulv ?
+            {
+                foundfreepos = true;
+                for (Player p : players) {
+                    if (p.getXpos() == x && p.getYpos() == y) // pladsen optaget af en anden
+                        foundfreepos = false;
+                }
+            }
+        }
+        pair p = new pair(x, y);
+        return p;
+    }
+
+    public static void updatePlayer(Player me, int delta_x, int delta_y, String direction) {
+        me.direction = direction;
+        int x = me.getXpos();
+        int y = me.getYpos();
+
+        if (Generel.board[y + delta_y].charAt(x + delta_x) == 'w') {
+            me.addPoints(-1); // Ramte en mur
+        }
+        else {
+            // Kollisions-detektion
+            Player p = getPlayerAt(x + delta_x, y + delta_y);
+            if (p != null) {
+                me.addPoints(10);
+                // Opdater den anden spiller
+                p.addPoints(-10);
+                pair pa = getRandomFreePosition();
+                p.setLocation(pa);
+                // BEMÆRK: Vi har fjernet Gui.movePlayerOnScreen herfra!
+            } else {
+                me.addPoints(1);
+            }
+
+            // Flyt spilleren i hukommelsen
+            pair newpos = new pair(x + delta_x, y + delta_y);
+            // BEMÆRK: Vi har fjernet Gui.movePlayerOnScreen herfra!
+            me.setLocation(newpos);
+        }
+    }
+
+    public static Player getPlayerAt(int x, int y) {
+        for (Player p : players) {
+            if (p.getXpos() == x && p.getYpos() == y) {
+                return p;
+            }
+        }
+        return null;
+    }
 }
